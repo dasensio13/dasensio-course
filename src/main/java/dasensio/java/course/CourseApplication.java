f089319d7dcccc5d9a998547a0dd9b51ecadd820
dasensio13 <dasensio13@gmail.com>
2017-02-17 20:14:32 +0100

    initial springboot project

---
+++ src/main/java/dasensio/java/course/CourseApplication.java
@@ -0,0 +1,12 @@
+package dasensio.java.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
---
+++ src/main/java/dasensio/java/course/ServletInitializer.java
@@ -0,0 +1,13 @@
+package dasensio.java.course;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(CourseApplication.class);
+	}
+
+}
